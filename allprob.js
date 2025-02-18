8f805752b8591d7b492a7d2f4e6a0a25207e9cbe
sufihasan <hasan6nh@gmail.com>
2025-02-18 10:37:14 +0600

    pb5 done and add to allpb

--- allprob.js
+++ allprob.js
@@ -88,4 +88,42 @@ function validProposal(person1, person2) {
 
 
 
+function calculateSleepTime(times) {
+    if (!Array.isArray(times)) {
+        return 'Invalid';
+    }
+    let sum = 0;
+    for (const time of times) {
+        if (typeof time !== 'number') {
+            return 'Invalid';
+        }
+        sum = sum + time;
+    }
+
+
+
+    let totalMin = parseInt(sum / 60);
+    let totalSec = sum % 60;
+    let totalHour = 0;
+
+
+    const totalTime = { hour: 0, minute: 0, second: 0 };
+
+    if (totalMin >= 60) {
+        totalHour = parseInt(totalMin / 60);
+        totalMin = totalMin % 60;
+
+    }
+
+    totalTime.hour = totalHour;
+    totalTime.minute = totalMin;
+    totalTime.second = totalSec;
+
+
+    return totalTime;
+
+}
+
+
+
 
